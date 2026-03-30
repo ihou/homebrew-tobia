2ea246cf0d65489434134a08e5b56e23c081e7f8
owen <i.hou@163.com>
2026-03-30 18:10:20 +0800

    chore: bump tobia-link to v0.1.4

--- Formula/tobia-link.rb
+++ Formula/tobia-link.rb
@@ -1,21 +1,21 @@
 class TobiaLink < Formula
   desc "Tobia local bridge for Codex session relay"
   homepage "https://github.com/ihou/tobia-link-releases"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ihou/tobia-link-releases/releases/download/v0.1.3/tobia-link_0.1.3_darwin_arm64.tar.gz"
-      sha256 "12c185e6f6e5aca103a543e2dda4cef93ea5d4b2c7a2b23a8fee7cee029758ad"
+      url "https://github.com/ihou/tobia-link-releases/releases/download/v0.1.4/tobia-link_0.1.4_darwin_arm64.tar.gz"
+      sha256 "45070dfca85503293466c3a0455f0a471d05374ec5bb4abadcd6ef7197c5f2ae"
     else
-      url "https://github.com/ihou/tobia-link-releases/releases/download/v0.1.3/tobia-link_0.1.3_darwin_amd64.tar.gz"
-      sha256 "45338e892eab84601be7e22bb98deb722ac9da84c25e778032cbd12ee472bf5b"
+      url "https://github.com/ihou/tobia-link-releases/releases/download/v0.1.4/tobia-link_0.1.4_darwin_amd64.tar.gz"
+      sha256 "04e7603c257e5c4471ddfb902f6f0a23ee8c245ecfc64faf6155f325bbfd4d52"
     end
   end
 
   on_linux do
-    url "https://github.com/ihou/tobia-link-releases/releases/download/v0.1.3/tobia-link_0.1.3_linux_amd64.tar.gz"
-    sha256 "38664864b94d56095a3cd14d6fd8aa58604036a4537a1d3cf377e9587797d6bf"
+    url "https://github.com/ihou/tobia-link-releases/releases/download/v0.1.4/tobia-link_0.1.4_linux_amd64.tar.gz"
+    sha256 "9ca20445fa2188fab9878c4dc45c9d2b64a40ffc6c9b671d6252f7a4badf6495"
   end
 
   def install
